09d291c960bd4df7d8faf964b83555aa9489017c
Lenin Acevedo <lenin.acevedo999@hotmail.com>
2024-07-30 09:11:29 -0600

    add service and test for service to connect to BigQuery

--- Angular/src/app/components/pages/home/home.component.ts
+++ Angular/src/app/components/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { MainService } from 'src/app/services/services/main.service';
 import { convertFileToDataUri } from 'src/app/shared/utils/utils.functions';
 
 @Component({
@@ -9,9 +11,28 @@ import { convertFileToDataUri } from 'src/app/shared/utils/utils.functions';
 export class HomeComponent {
 
   claims: any = [
-    { status: 'Open', program: 'UI', bye: '12/30/2023', paidDate: 1016, mba: 4918, wba: 189, rba: 3898},
-    { status: 'Expired', program: 'UI', bye: '03/19/2022', paidDate: 512, mba: 4524, wba: 174, rba: 4012},
-    ]
+    { status: 'Open', program: 'UI', bye: '12/30/2023', paidDate: 1016, mba: 4918, wba: 189, rba: 3898 },
+    { status: 'Expired', program: 'UI', bye: '03/19/2022', paidDate: 512, mba: 4524, wba: 174, rba: 4012 },
+  ];
+
+  constructor(private _mainSvc: MainService,
+    private toastr: ToastrService
+  ) {}
+
+  ngOnInit() {
+    this.getDatFromBigQuery();
+  }
+
+  getDatFromBigQuery= async() => {
+    // this._mainSvc.loginBigQuery();
+    // this._mainSvc.getAccessTokenBigQuery();
+    // (await this._mainSvc.getData()).subscribe((resp: any) => {
+    //   console.log(resp)
+    // });
+    (await this._mainSvc.getDataPython()).subscribe((resp: any) => {
+      console.log(resp)
+    });
+  }
 
   onFileSelected(event: any) {
     const input = event.target as HTMLInputElement;
--- Angular/src/app/services/services/main.service.ts
+++ Angular/src/app/services/services/main.service.ts
@@ -1,9 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Resultado } from 'src/app/shared/models/general.model';
 import { environment } from 'src/environments/environment';
-
 @Injectable({
   providedIn: 'root'
 })
@@ -13,6 +12,10 @@ export class MainService {
 
   constructor(private http: HttpClient,
   ) { }
+
+  getDataPython = async () => {
+    return this.http.get<any>('http://127.0.0.1:5000/api/data');
+  }
 
   login(cc: any): Observable<Resultado> {
       return this.http.post<Resultado>(this.UrlBase + '/Servicio/login', cc);
